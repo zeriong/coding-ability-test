351c3e70f0d126002da76454955d88910e1cdce7
zeriong <115396103+zeriong@users.noreply.github.com>
2024-08-02 18:23:23 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 2.51 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -0,0 +1,15 @@
+function solution(my_strings, parts) {
+    var answer = '';
+    
+    str = "abcdef"
+    console.log(str.slice(0,4 + 1))
+    
+    for (let i = 0; i < my_strings.length; i++) {
+        const currentString = my_strings[i];
+        const [start, end] = parts[i];
+        if (start == end) answer += currentString[start];
+        else answer += my_strings[i].slice(start, end + 1);
+    }
+    
+    return answer;
+}
