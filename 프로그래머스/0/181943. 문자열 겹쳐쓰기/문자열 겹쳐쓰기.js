5c2c8c89d314fe1d13959176eef8e4087da6dd0b
zeriong <115396103+zeriong@users.noreply.github.com>
2024-08-07 12:34:31 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -0,0 +1,18 @@
+function solution(my_string, overwrite_string, s) {
+    var answer = '';
+    
+    const overLeng = overwrite_string.length;
+    const strLeng = my_string.length;
+    
+    for (let i = 0; i < strLeng; i++) {
+        if (i == s) {
+            for (let j = 0; j < overLeng; j++) {
+                answer += overwrite_string[j];
+                i += 1;
+            }
+        }
+        answer += my_string[i] || "";
+    }
+    
+    return answer;
+}
