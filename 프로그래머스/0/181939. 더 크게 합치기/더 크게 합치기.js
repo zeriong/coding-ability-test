038c497e3eb0468675ec6788b90fa6bf91947891
zeriong <115396103+zeriong@users.noreply.github.com>
2024-08-08 10:02:08 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -0,0 +1,6 @@
+function solution(a, b) {
+    const caseA = Number(String(a) + String(b));
+    const caseB = Number(String(b) + String(a));
+    if (caseA === caseB) return caseA;
+    return Math.max(caseA, caseB);
+}
