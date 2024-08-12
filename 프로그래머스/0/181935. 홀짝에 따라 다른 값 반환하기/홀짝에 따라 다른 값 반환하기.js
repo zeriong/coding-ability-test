0ccecbb93708f3e154f75c75ec696eac16732ccf
zeriong <115396103+zeriong@users.noreply.github.com>
2024-08-12 13:44:44 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.15 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,11 @@
+const solution = (n) => {
+    let answer = 0;
+    for (let i = 1; i <= n; i++) {
+        if (n % 2 == 0) {
+            if (i % 2 == 0) answer += i * i;
+        } else {
+            if (i % 2 > 0) answer += i;
+        }
+    }
+    return answer;
+}
