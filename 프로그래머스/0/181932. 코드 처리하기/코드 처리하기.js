9d353496ba2c3009d56319cd1cc7d02d7715c164
zeriong <115396103+zeriong@users.noreply.github.com>
2024-08-14 20:08:08 +0900

    [level 0] Title: 코드 처리하기, Time: 9.12 ms, Memory: 39.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,23 @@
+function solution(code) {
+    var answer = "";
+    let mode = "0";
+    
+    for (let i = 0; i < code.length; i++) {
+        const word = code[i];
+        
+        if (word == "1" || word == "0") {
+            if (mode == "0" && word == "1") mode = "1";
+            else if (mode == "1" && word == "1") mode = "0";
+            continue;
+        }
+        
+        if (
+            (mode == "0") && (i % 2 == 0) ||
+            (mode == "1") && (i % 2 > 0)
+        ) {
+            answer += word;
+        }
+    }
+
+    return answer ? answer : "EMPTY";
+}
