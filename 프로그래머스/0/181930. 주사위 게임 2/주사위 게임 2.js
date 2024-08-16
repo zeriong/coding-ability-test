a4c3b22d8494ba6cc61da531f8c634b409918f3e
zeriong <115396103+zeriong@users.noreply.github.com>
2024-08-16 15:52:38 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,11 @@
+function solution(a, b, c) {
+    var answer = 0;
+    
+    const cnt = Number(a == b) + Number(b == c) + Number(a == c)
+    
+    if (cnt >= 0) answer = a + b + c;
+    if (cnt >= 1) answer = answer * (a**2 + b**2 + c**2);
+    if (cnt >= 3) answer = answer * (a**3 + b**3 + c**3);
+    
+    return answer;
+}
