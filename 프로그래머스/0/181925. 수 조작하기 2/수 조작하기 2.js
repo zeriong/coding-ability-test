6ffaf6db801ed1ac546b99d2c78b0621bd23a7c3
zeriong <115396103+zeriong@users.noreply.github.com>
2024-08-21 20:18:56 +0900

    [level 0] Title: 수 조작하기 2, Time: 17.91 ms, Memory: 42.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,9 @@
+function solution(numLog) {
+    var answer = '';
+    const obj = { "1": "w", "-1": "s", "10": "d", "-10": "a" };
+    for (let i = 1; i < numLog.length; i++) {
+        const calc = numLog[i] - numLog[i - 1];
+        answer += obj[calc];
+    }
+    return answer;
+}
