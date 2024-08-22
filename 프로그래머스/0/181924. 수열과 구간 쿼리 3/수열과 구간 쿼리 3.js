cc8c5333464139f92b7ace804ff6160be974de82
zeriong <115396103+zeriong@users.noreply.github.com>
2024-08-22 19:30:19 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 1.57 ms, Memory: 34.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -0,0 +1,9 @@
+function solution(arr, queries) {
+    var answer=  [...arr];
+    queries.forEach(([i, j]) => {
+        answer[i] = arr[j];
+        answer[j] = arr[i];
+        arr = [...answer];
+    })
+    return answer;
+}
