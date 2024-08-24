5cd07e71eb1ab48b2573bc02d22cf01364f2fca8
zeriong <115396103+zeriong@users.noreply.github.com>
2024-08-24 16:20:58 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 4.21 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -0,0 +1,14 @@
+function solution(arr, queries) {
+    for (let i = 0; i < queries.length; i++) {
+        const s = queries[i][0];
+        const e = queries[i][1];
+        const k = queries[i][2];
+        
+        for (let j = s; j <= e; j++) {
+            const jk = j * k;
+            if (jk == 0 || j % k == 0) arr[j]++;
+        }
+    }
+
+    return arr;
+}
