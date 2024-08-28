cbdec52070be5ce9de0c48c05b2813c37b01a233
zeriong <115396103+zeriong@users.noreply.github.com>
2024-08-28 09:06:40 +0900

    [level 0] Title: 배열 만들기 2, Time: 1714.08 ms, Memory: 63.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -0,0 +1,16 @@
+function solution(l, r) {
+    var answer = [];
+    
+    for (let i = l; i <= r; i++) {
+        const splitArr = String(i).split('');
+        let filtered = [];
+        
+        for (let j = 0; j < splitArr.length; j++) {
+            filtered = splitArr.filter(item => item.includes("5") || item.includes("0"));
+        }
+        
+        if (filtered.length == splitArr.length) answer.push(i);
+    }
+    
+    return answer.length ? answer : [-1];
+}
