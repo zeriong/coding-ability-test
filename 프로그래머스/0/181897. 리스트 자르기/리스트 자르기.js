f2b25a1db3c888a67e26bce033c77295e6c877ce
zeriong <115396103+zeriong@users.noreply.github.com>
2024-10-21 22:11:20 +0900

    [level 0] Title: 리스트 자르기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,11 @@
+function solution(n, slicer, num_list, answer = []) {
+    if (n == 1) return num_list.slice(0, slicer[1] + 1);
+    if (n == 2) return num_list.slice(slicer[0], num_list.length);
+    if (n == 3) return num_list.slice(slicer[0], slicer[1] + 1);
+    
+    const arr = num_list.slice(slicer[0], slicer[1] + 1);
+    for (let i = 0; i < arr.length; i++) {
+        if (i % slicer[2] == 0) answer.push(arr[i]);
+    }
+    return answer;
+}
