7f255b0880bc17d17c452d9e5f689877663ad9a3
zeriong <115396103+zeriong@users.noreply.github.com>
2024-12-01 23:52:17 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -0,0 +1,13 @@
+function solution(str_list) {
+    var answer = [];
+    const len = str_list.length;
+    for (let i = 0; i < len; i++) {
+        if (str_list[i] == "l") {
+            return str_list.slice(0, i);
+        }
+        if (str_list[i] == "r") {
+            return str_list.slice(i+1, len);
+        }
+    }
+    return answer;
+}
