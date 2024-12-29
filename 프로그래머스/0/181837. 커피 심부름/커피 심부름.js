21820a9fcb9ee5f240b1d5826ea05db070ac09d7
zeriong <115396103+zeriong@users.noreply.github.com>
2024-12-29 23:22:30 +0900

    [level 0] Title: 커피 심부름, Time: 0.70 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
@@ -0,0 +1,22 @@
+function solution(order) {
+    
+    const menu = [
+        { 
+            name: "anything",
+            cost: 4500
+        },
+        { 
+            name: "americano",
+            cost: 4500
+        },
+        { 
+            name: "cafelatte",
+            cost: 5000
+        },
+    ];
+    
+    return order.reduce((a,c) => {
+        const findOne = menu.find(m => c.includes(m.name));
+        return a += findOne.cost
+    }, 0);
+}
