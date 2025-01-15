dc32d93a758f194a8a68e8a79a926ccd032ea56a
zeriong <115396103+zeriong@users.noreply.github.com>
2025-01-15 12:29:57 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.65 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -0,0 +1,18 @@
+function solution(arr) {
+    let isDiff = false;
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (isDiff) break;
+        
+        const cur = arr[i];
+        
+        for (let j = 0; j < cur.length; j++) {
+            if (arr[i][j] != arr[j][i]) {
+                isDiff = true;
+                break
+            }
+        }
+    }
+    
+    return Number(!isDiff);
+}
