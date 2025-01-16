7687715365900badf3000740932a72bae5b5473d
zeriong <115396103+zeriong@users.noreply.github.com>
2025-01-16 20:03:27 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.39 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.js
@@ -0,0 +1,23 @@
+function solution(arr, flag) {
+    var answer = [];
+    
+    const increaseArr = (value) => {
+        const length = value * 2;
+        const arr = [];
+        for(let i = 0; i < length; i++) {
+            arr.push(value)
+        }
+        return arr;
+    }
+    
+    const decreaseArr = (value) => {
+        for(let i = 0; i < value; i++) answer.pop();
+    }
+    
+    arr.forEach((v, i) => {
+        if (flag[i]) answer.push(...increaseArr(v));
+        else decreaseArr(v);
+    })
+    
+    return answer;
+}
