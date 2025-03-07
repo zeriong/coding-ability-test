5d93eb7b42b383e36071b86f91b79f6ea06ad4e3
zeriong <115396103+zeriong@users.noreply.github.com>
2025-03-07 18:23:04 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -2,5 +2,5 @@ const solution = (num_list) => {
     const length = num_list.length;
     const [last, beforeLast] = [num_list[length - 1], num_list[length - 2]];
     const target = (last > beforeLast) ? (last - beforeLast) : (last * 2);
-    return [...num_list, target];
+    return [...num_list, target]; 
 };
