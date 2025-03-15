b4c609730cca6b81e7853ff096b1e51ba607f15a
zeriong <115396103+zeriong@users.noreply.github.com>
2025-03-15 22:47:56 +0900

    [level 0] Title: 배열 만들기 3, Time: 3.89 ms, Memory: 42.3 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,3 +1,1 @@
-const solution = (a,_i,_a=[])=>{_i.forEach(([s,e])=>{for(let i=s;i<=e;i++)_a.push(a[i])});return _a};
-
-// 꾸역꾸역 한 줄 코딩
+const solution = (a,_i,_a=[])=>{_i.forEach(([s,e])=>{for(let i=s;i<=e;i++)_a.push(a[i])});return _a}; 
