fda51f3c704f3840d202d84e7fb2471be7aa8b52
zeriong <115396103+zeriong@users.noreply.github.com>
2025-03-26 22:11:48 +0900

    [level 0] Title: 문자열 묶기, Time: 6.19 ms, Memory: 45 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -3,5 +3,5 @@ const solution=(arr, ans=[]) => {
         var len = v.length;
         ans[len] ? ans[len] += 1 : ans[len] = 1;
     });
-    return Math.max(...ans.filter(v=>v));
+    return Math.max(...ans.filter(v=>v)); 
 }
