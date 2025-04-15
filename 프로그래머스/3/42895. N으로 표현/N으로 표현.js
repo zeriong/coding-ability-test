c155aa3651c314c9c1763e1c40270962fa88ef4d
zeriong <115396103+zeriong@users.noreply.github.com>
2025-04-15 19:14:53 +0900

    [level 3] Title: N으로 표현, Time: 24.32 ms, Memory: 39.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -0,0 +1,25 @@
+function solution(N, number) {
+    let arr = new Array(8).fill().map((_, i) => {
+        return new Set([ Number(`${N}`.repeat(i + 1)) ]);
+    });
+    
+    const calcArr = (v1, v2, i) => {
+        arr[i].add(v1 + v2);
+        arr[i].add(v1 - v2);
+        arr[i].add(v1 * v2);
+        arr[i].add(parseInt(v1 / v2));
+    }
+    
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < i; j++) {
+            for (let v1 of arr[j]) {
+                for (let v2 of arr[i - j - 1]) {
+                    calcArr(v1, v2, i);
+                }
+            }
+        } 
+        if (arr[i].has(number)) return i + 1;
+    }
+    
+    return -1;
+}
