00b561f57a3c47b6af17a33a4765e4784ee3ec47
zeriong <115396103+zeriong@users.noreply.github.com>
2025-04-22 10:09:53 +0900

    [level 2] Title: 오픈채팅방, Time: 170.69 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -0,0 +1,29 @@
+function solution(record) {
+    var answer = [];
+    let map = new Map();
+    let idsAndActs = [];
+    
+    function createMessage(act, user) {
+        if (act == 'Enter') return `${user}님이 들어왔습니다.`;
+        if (act == 'Leave') return `${user}님이 나갔습니다.`;
+    }
+    
+    record.map(val => {
+        const d = val.split(" ");
+        if (d[0] == 'Enter' || d[0] == 'Leave') {
+            idsAndActs.push([d[0], d[1]]); 
+        }
+        if (d[0] != 'Leave') {
+            map.set(d[1], d[2]);
+        }
+    });
+    
+    idsAndActs.map(val => {
+        const [act, id] = val;
+        const nick = map.get(id);
+        const txt = createMessage(act, nick);
+        answer.push(txt)
+    })
+    
+    return answer;
+}
