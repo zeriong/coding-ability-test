2dfee0abe29658f8e8bf100386b0d2374a882683
zeriong <115396103+zeriong@users.noreply.github.com>
2025-07-30 18:18:22 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -3,5 +3,5 @@ function solution(p,m,c) {
     for (let i = 1; i <= c; i++) {
         remain -= i * p
     }
-    return remain < 0 ? Math.abs(remain) : 0;
+    return remain < 0 ? Math.abs(remain) : 0; 
 }
